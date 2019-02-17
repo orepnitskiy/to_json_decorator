1cb8d3c601b255f2bb8f10e74b87073d8040339d
Oleg Repnitskiy <44697305+pharaohandtrasher@users.noreply.github.com>
2019-02-17 23:53:23 +0500

    Delete to_json.py

--- to_json.py
+++
@@ -1,9 +0,0 @@
-from functools import wraps
-def to_json(func):
-	@wraps(func)
-	def wrapped(*args,**kwargs):
-		result=func(*args,**kwargs)
-		c=json.JSONEncoder().encode(result)
-		print(c)
-		return c
-	return wrapped
